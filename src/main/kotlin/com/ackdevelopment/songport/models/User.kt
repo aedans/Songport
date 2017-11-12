e1d65cc2dbb2d20c17af8f0da5a5c827196ff7d1
tkaden4 <thomas.kaden4@outlook.com>
2017-11-12 02:14:43 -0800

    Fix spotify auth

--- src/main/kotlin/com/ackdevelopment/songport/models/User.kt
+++ src/main/kotlin/com/ackdevelopment/songport/models/User.kt
@@ -5,7 +5,8 @@ import java.io.Serializable
 data class User(
         val _id: String,
         val password: String,
-        val playlistIds: List<String>
+        val playlistIds: List<String>,
+        val spotifyAuthCode: String?
 ): Serializable {
     val title get() = _id
 }
