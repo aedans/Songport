0732aa3a521cba9ead7296625b6ca99d3f5eef51
tkaden4 <thomas.kaden4@outlook.com>
2017-11-12 01:51:50 -0800

    Update links and serviceauth

--- src/main/kotlin/com/ackdevelopment/songport/sites/Util.kt
+++ src/main/kotlin/com/ackdevelopment/songport/sites/Util.kt
@@ -4,14 +4,14 @@ import kotlinx.html.HEAD
 import kotlinx.html.link
 
 fun HEAD.links() {
-    link(rel = "stylesheet", type = "text/css", href = "styles/normalize.css") {
+    link(rel = "stylesheet", type = "text/css", href = "/styles/normalize.css") {
     }
 
-    link(rel = "stylesheet", type = "text/css", href = "styles/skeleton.css") {
+    link(rel = "stylesheet", type = "text/css", href = "/styles/skeleton.css") {
 
     }
 
-    link(rel = "stylesheet", type = "text/css", href = "styles/songport.css") {
+    link(rel = "stylesheet", type = "text/css", href = "/styles/songport.css") {
 
     }
 }
