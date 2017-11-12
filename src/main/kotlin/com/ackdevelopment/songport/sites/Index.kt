85031e771b3edf53498b5ef595c18b982ea62b22
tkaden4 <thomas.kaden4@outlook.com>
2017-11-12 04:09:57 -0800

    remove faulty code

--- src/main/kotlin/com/ackdevelopment/songport/sites/Index.kt
+++ src/main/kotlin/com/ackdevelopment/songport/sites/Index.kt
@@ -2,16 +2,12 @@ package com.ackdevelopment.songport.sites
 
 import kotlinx.html.*
 import org.jetbrains.ktor.http.ContentType
-import org.jetbrains.ktor.locations.get
-import org.jetbrains.ktor.locations.location
 import org.jetbrains.ktor.response.respondText
+import org.jetbrains.ktor.routing.get
 import org.jetbrains.ktor.routing.Routing
 
-@location("/")
-class Index
-
 fun Routing.index() {
-    get<Index> {
+    get("/") {
         call.respondText(getIndexHtml(), ContentType.Text.Html)
     }
 }
