0c40c5dfa84d13f0ba8e791068c70b4b55429ae1
Aedan <aedan.smith@gmail.com>
2017-11-11 22:33:21 -0800

    Fix register.html

--- src/main/kotlin/com/ackdevelopment/songport/users/Register.kt
+++ src/main/kotlin/com/ackdevelopment/songport/users/Register.kt
@@ -1,12 +1,16 @@
 package com.ackdevelopment.songport.users
 
+import com.ackdevelopment.songport.SongportSession
 import com.ackdevelopment.songport.digest
 import com.ackdevelopment.songport.models.User
 import com.ackdevelopment.songport.putUser
 import org.jetbrains.ktor.locations.location
 import org.jetbrains.ktor.locations.post
 import org.jetbrains.ktor.request.receive
+import org.jetbrains.ktor.response.respondRedirect
 import org.jetbrains.ktor.routing.Routing
+import org.jetbrains.ktor.sessions.sessions
+import org.jetbrains.ktor.sessions.set
 import org.jetbrains.ktor.util.ValuesMap
 
 @location("/register")
@@ -19,5 +23,9 @@ fun Routing.register() {
         val password = registration["password"]!!
 
         putUser(User(username, digest(password), emptyList()))
+
+        call.sessions.set(SongportSession(password))
+
+        call.respondRedirect("/user")
     }
 }
