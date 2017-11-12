78519cc7c4b919af5a8d64fd7931f4a3ae3bf549
Aedan <aedan.smith@gmail.com>
2017-11-12 05:25:33 -0800

    Fix amazing names

--- src/main/kotlin/com/ackdevelopment/songport/Util.kt
+++ src/main/kotlin/com/ackdevelopment/songport/Util.kt
@@ -1,11 +1,11 @@
 package com.ackdevelopment.songport
 
+import org.jetbrains.ktor.util.encodeBase64
 import org.jetbrains.ktor.util.getDigestFunction
 import java.io.File
-import java.nio.charset.Charset
 
 fun String.readText() = File(this).readText()
 
 val songportURL = "198.199.116.233"
 
-val digest = { it: String -> getDigestFunction("MD5", "")(it).toString(Charset.defaultCharset()) }
+val digest = { it: String -> encodeBase64(getDigestFunction("MD5", "")(it)) }
